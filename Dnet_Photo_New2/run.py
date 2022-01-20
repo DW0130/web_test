8ee3aab17b7b9ef4aa2aff606645429095a79646
DaWoon <dw010130@kakao.com>
2022-01-20 16:22:07 +0900

    초기파일

---
+++ Dnet_Photo_New2/run.py
@@ -0,0 +1,232 @@
+#This is a python
+from flask import Flask,render_template,request,send_file,redirect,session,flash
+
+from werkzeug import secure_filename
+import os,sys
+import time
+import hashlib
+import pymysql
+import threading
+import openpyxl
+import statistics
+from openpyxl import Workbook, drawing 
+
+DELETE = "rm -f /home/pi/Desktop/Dnet_WEB_/Dnet_Photo-management/Web_server2"
+str2 = '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918'
+
+import json
+
+app = Flask(__name__,template_folder='templates')
+app.secret_key = 'asdf'
+app.config["SECRET_KEY"] = "ABCD"
+
+
+def shutdown_server():
+    
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+
+@app.route("/")
+def main():
+    return render_template('login.html')
+
+@app.route("/sign_up") #새로운 화면 만들기
+def sign_up():
+    return render_template('sign_up.html')
+
+@app.route("/login") #새로운 화면 만들기
+def login():
+    return render_template('login.html')
+
+
+@app.route("/popup")
+def popup():
+    return render_template('popup.html')
+
+@app.route("/idd",methods=['POST'])
+def idd():
+    id=""
+    if request.method == 'POST':
+        id = request.form.get('id')
+   
+    session['iddd'] = id
+    return ('', 204)
+
+@app.route("/edit",methods=['POST'])
+def editspeed():
+    ed=""
+    if request.method == 'POST':
+        ed = request.form.get('ed')
+    print(ed)
+    fn = session['iddd']
+    print(fn)
+    f = open("/home/pi/Desktop/Dnet_Photo_New/static/speeddata/speedlimit_"+fn+".txt", 'w')   
+    f.write(ed)
+    f.close()
+    return ('', 204)
+
+@app.route("/list")
+def listmain():
+    if (session.get('login')) :
+        return render_template('listmain.html')
+    else :
+        flash("로그인이 필요합니다")
+        return redirect('/')
+
+@app.route("/analysis")
+def analysis():
+    if (session.get('login')) :
+        return render_template('analysis.html')
+    else :
+        flash("로그인이 필요합니다")
+        return redirect('/')
+
+@app.route("/p2",methods=['POST'])
+def realmain():
+    num=""
+    num2=""
+    if request.method == 'POST':
+        num = request.form.get('num')
+        num2 = request.form.get('num2')
+        result = hashlib.sha256(num2.encode()).hexdigest()
+        if (num == "dnet" and num2 == "admin") or (num =='a' and result == str2):
+            session['login'] = 1
+            return redirect('/list')
+        else:
+            flash("아이디 또는 비밀번호가 일치하지 않습니다")
+            return render_template('login.html')
+
+@app.route("/re",methods=['POST'])
+def remove():
+    if request.method == 'POST':
+        a = request.form.get('url')
+        DELETE_ORDER = DELETE + a
+        print(DELETE_ORDER)
+        os.system(DELETE_ORDER)
+    return redirect('/list')
+
+@app.route("/searchDB",methods=['POST'])
+def searchDB():
+    if request.method == 'POST':
+        i = request.get_json()
+        
+        DB = pymysql.connect(host='192.168.0.43',user='root',password='1234',db='speeddb',charset='utf8')  
+        sql = DB.cursor()
+        sql.execute("select LCT,SPD,TME,PHT from speeddb.CarDB where TME between '"+i.get('startdate')+"' and '"+i.get('enddate')+"' AND SPD >"+i.get('lim')+" AND LCT ='"+i.get('location')+"' ORDER BY TME DESC")
+        data = json.dumps(sql.fetchall(),default=str)
+        return data
+
+@app.route("/searchDBAn",methods=['POST'])
+def searchDBAn():
+    if request.method == 'POST':
+        i = request.get_json()
+        
+        DB = pymysql.connect(host='192.168.0.43',user='root',password='1234',db='speeddb',charset='utf8')  
+        sql = DB.cursor()
+        sql.execute("select LCT,SPD,HOUR(TME) from speeddb.CarDB where TME between '"+i.get('startdate')+"' and '"+i.get('enddate')+"' AND SPD >"+i.get('lim')+" ORDER BY TME DESC")
+        data = json.dumps(sql.fetchall(),default=str)
+        return data
+
+@app.route("/excelexport",methods=['POST'])
+def excelexport():
+    if request.method == 'POST':
+        i = request.get_json()
+        
+
+        DB = pymysql.connect(host='192.168.0.43',user='root',password='1234',db='speeddb',charset='utf8')  
+        try:
+            with DB.cursor() as curs:
+                sql = "select SPD,TME from speeddb.CarDB where TME between '"+i.get('startdate')+"' and '"+i.get('enddate')+"' AND SPD >"+i.get('lim')+" AND LCT ='"+i.get('location')+"' ORDER BY TME DESC"
+
+                sql2 = "select PHT from speeddb.CarDB where TME between '"+i.get('startdate')+"' and '"+i.get('enddate')+"' AND SPD >"+i.get('lim')+" AND LCT ='"+i.get('location')+"' ORDER BY TME DESC"
+
+                curs2 = DB.cursor()
+
+                curs.execute(sql)
+                curs2.execute(sql2)
+                rs = curs.fetchall()
+                rs2 = curs2.fetchall()
+
+                wb = Workbook()
+                ws = wb.active
+
+                #첫행 입력
+                ws.append(('번호','이름','d','d'))
+
+                #DB 모든 데이터 엑셀로
+                index = 2
+                for row in rs:
+                    ws.append(row)
+                    data4 = rs2[index-2][0]
+                    img = openpyxl.drawing.image.Image(data4)
+                    img.height = 305.5
+                    img.width= 405.5
+                    a = 'D'+str(index)
+                    ws.add_image(img,a)
+                    print("index")
+                    print(a)
+                    index = index+1
+                wb.save("/home/pi/Desktop/Dnet_Photo_New/static/exli.xlsx")
+        finally:
+            DB.close()
+            wb.close()
+        return '', 204
+
+
+
+@app.route("/testpost",methods=['POST'])
+def testpost(): #연습용Post
+    Realid = 1234
+    Realpw = 5678
+    if request.method == 'POST':
+        i = request.get_json()
+        recvdata = i.get('numder')
+        Realid = i.get('ID')
+        Realpw = i.get('PW')
+
+        if Realid == '1234' and Realpw == '5678':
+            return json.dumps("{'data' : 로그인 되었습니다.}")
+        else:
+            return json.dumps("{'data' : 비닐번호가 틀렸습니다.}")
+       
+newid = ''
+newpw = ''
+@app.route("/Signup_post",methods=['POST']) #post방식으로 testpost_1주소(함수)로가서 진행
+def Signup_post(): #연습용Post
+    global newid
+    global newpw
+
+    if request.method == 'POST':
+        i = request.get_json()
+        recvdata = i.get('numder')
+        newid = i.get('New_ID')
+        newpw = i.get('New_PW')
+
+        return json.dumps ("{'data' : '회원가입이 되었습니다.'}")
+
+
+@app.route("/testpost_1",methods=['POST']) #post방식으로 testpost_1주소(함수)로가서 진행
+def testpost_1(): #연습용Post
+    global newid
+    global newpw
+    if request.method == 'POST':
+        i = request.get_json()
+        Realid = i.get('ID')
+        Realpw = i.get('PW')
+
+        if newid == i.get('ID') and newpw == i.get('PW'):
+            return json.dumps("{'data' : 로그인 되었습니다.}")
+        else:
+            return json.dumps("{'data' : 비밀번호가 틀렸습니다.}")
+
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0',port=5000, threaded=True)
+    
+    #os.execl(sys.executable, sys.executable, *sys.argv)
+
---
+++ Dnet_Photo_New2/static/test.js
@@ -0,0 +1,147 @@
+// window.addEventListener('DOMContentLoaded', function() { 
+//     document.getElementById("btn").addEventListener('click',hello);
+
+//     function hello(){
+//         var a = document.getElementById("in").value
+//         var b = document.getElementById("in_1").value
+//         var c = Number(a) + Number(b)
+//         document.getElementById("in_2").value = c
+//       alert(c);
+//     }
+// })
+
+
+
+function search(id, pw){
+
+    var ID = id
+    var PW = pw
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/testpost", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        ID : id,
+        PW : pw
+
+    }));
+    xhr.onload = function() {
+    var data = JSON.parse(this.responseText)
+    console.log(data);
+
+
+    }
+
+}
+
+
+function login(){
+    var id = document.getElementById('ID').value
+    var pw = document.getElementById('PW').value
+
+    console.log(id)
+    console.log(pw)
+
+    search(id, pw)
+}
+
+function search_test(N_ID, N_PW){
+
+    var ID = N_ID
+    var PW = N_PW
+
+    var xhr = new XMLHttpRequest(); // XMLHttpRequest = 서버와 상호작용
+    xhr.open("POST", "/testpost_1", true); // 서버로부터 데이터 받아오기위한 객체 "open()함수를 통해 요청초기화"
+    xhr.setRequestHeader('Content-Type', 'application/json'); //HTTP요청헤더 값 설정  "serRequestheader()함수설정"
+    xhr.send(JSON.stringify({   // send()함수를 통해 요청전송 "setRequestHeader() 함수로 JSON이라고 설정하고, send() 함수로 서버에 보내는 데이터를 JSON 문자열변경"
+
+        ID : N_ID,
+        PW : N_PW
+
+    }));
+    xhr.onload = function() {
+    var data = JSON.parse(this.responseText)
+    console.log(data);
+
+
+    }
+
+}
+
+function login_test(){
+    var N_ID = document.getElementById('N_UserID').value
+    var N_PW = document.getElementById('N_UserPW').value
+
+    console.log(N_ID)
+    console.log(N_PW)
+
+    search_test(N_ID, N_PW)
+}
+
+function mail_bt(){
+    
+    console.log("mail_버튼눌림")
+}
+
+function cafe_bt(){
+    
+    console.log("cafe_버튼눌림")
+}
+function blog_bt(){
+    
+    console.log("blog_버튼눌림")
+}
+function know_bt(){
+    
+    console.log("know_버튼눌림")
+}
+function shop_bt(){
+    
+    console.log("shop_버튼눌림")
+}
+function pay_bt(){
+    
+    console.log("pay_버튼눌림")
+}
+
+function Sign_up(){
+
+    var New_UserID = document.getElementById('NEW_User_ID').value
+    var New_UserPW = document.getElementById('NEW_User_PW').value
+    var New_UserNAME = document.getElementById('NAME').value
+    var New_User_Phone = document.getElementById('Phone_Number').value
+
+    console.log(New_UserID)
+    console.log(New_UserPW)
+    console.log(New_UserNAME)
+    console.log(New_User_Phone)
+
+    search_Signup(New_UserID, New_UserPW, New_UserNAME, New_User_Phone)
+}
+
+function search_Signup(New_UserID, New_UserPW, New_UserNAME, New_User_Phone){
+
+    var New_ID = New_UserID
+    var New_PW = New_UserPW
+    var New_NAME = New_UserNAME
+    var New_Phone = New_User_Phone
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/Signup_post", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+
+        New_ID : New_UserID,
+        New_PW : New_UserPW,
+        New_NAME : New_UserNAME,
+        New_Phone : New_User_Phone
+
+    }));
+    xhr.onload = function() {
+    var data = JSON.parse(this.responseText)
+    console.log(data);
+
+
+    }
+
+}
